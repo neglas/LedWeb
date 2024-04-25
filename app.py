65ddca1023a26bb5ff0b603e48cd8fd7fe50cd7b
Neglas <hansson.niclas.hansson@gmail.com>
2024-04-25 21:35:05 +0200

    button does something

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+def MyFunc():
+    return "Button press success!"
+    
 @app.route('/')
-
 def index():
     return render_template('index.html')
+
+@app.route('/execute', methods=['POST'])
+def ExecuteFunction():
+    if request.method == 'POST':
+        result = MyFunc()
+        return result
 
 if __name__=='__main__':
     app.run(debug=True, host='0.0.0.0')
